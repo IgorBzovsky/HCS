960cdaa978e516a7f3eaee1ac833caef2e758e28
Igor Bzovsky <igor77188@gmail.com>
2018-02-01 21:31:14 +0200

    Initial solution structure

---
+++ HCS.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace HCS.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<HcsDbContext>
+    {
+        public HcsDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<HcsDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=hcsdb;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(HcsDbContext)
+                .GetTypeInfo().Assembly.GetName().Name));
+            return new HcsDbContext(builder.Options);
+        }
+    }
+}
---
+++ HCS.Data/HcsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HCS.Core.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCS.Data
+{
+    public class HcsDbContext : IdentityDbContext<User>
+    {
+        public HcsDbContext(DbContextOptions<HcsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
